bcf51f95579f08b8816b89f7809da450d859b1be
Usman <usmaniftikhar.ciit@gmail.com>
2022-03-28 16:24:29 +0500

    V1

---
+++ app/src/main/java/pk/gop/pfa/nutrition/nutrition/MainActivity.java
@@ -0,0 +1,16 @@
+package pk.gop.pfa.nutrition.nutrition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.androidbrowserhelper.trusted.LauncherActivity;
+
+public class MainActivity extends LauncherActivity {
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }*/
+}
